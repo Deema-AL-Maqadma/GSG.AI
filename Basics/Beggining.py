0eba46239269f027f33e4bc488da8ccf6c08773d
Deema-AL-Maqadma <dema.maqadma@gmail.com>
2025-09-18 13:41:10 +0300

    The Beggining

---
+++ Basics/Beggining.py
@@ -0,0 +1,22 @@
+print("*** Deema Mohammed AL-Maqadma ***")
+print("*********************************************")
+name = input("Enter your name: ")
+print("Hello", name)
+print("Hello", input("Enter your name: "))# اختصار الكود
+
+print("*********************************************")
+#Q1/Read two numbers and calculate their sum and average.
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+print("Sum:", a + b)
+print("Avarage :", (a+b)/2)
+print("*********************************************")
+#Q2/Convert a temperature from Celsius to Fahrenheit using the formula: F = C * 9/5 + 32
+temp=float(input("Enter the temperature : "))
+print("Fahrenheit =",(temp*9/5)+32)
+print("*********************************************")
+#Q3/Calculate and print the perimeter of a rectangle from user input (2 × (length + width)).
+l=int(input("Enter length : "))
+w=int(input("Enter width : "))
+print("The perimeter of a rectangle = ",2*(l+w))
+print("*********************************************")
